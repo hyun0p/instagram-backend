6f6aefa289d66a6752518c689ea407b5f9c454c3
h000 <phy9250@gmail.com>
2021-09-06 13:46:51 +0900

    initial commit

---
+++ src/test/java/com/clonecoding/instagrambackend/InstagramBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clonecoding.instagrambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/clonecoding/instagrambackend/controller/testControllerTest.java
@@ -0,0 +1,17 @@
+package com.clonecoding.instagrambackend.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class testControllerTest {
+
+    @Test
+    void tt() {
+
+    }
+
+    @Test
+    void test1() {
+    }
+}
